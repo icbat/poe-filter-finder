897cd006440f5248085fab1ea60ff9384c249e25
icbat <cobb.evan@gmail.com>
2015-05-01 22:11:31 -0500

    Verifying the full path exists

--- finder.go
+++ finder.go
@@ -13,10 +13,26 @@ func main() {
 	fmt.Println("Found base path of: " + basePath)
 
 	fmt.Println("Verifying directories")
+	path := basePath + "/Documents/My Games/Path of Exile"
+	if !direxists(path) {
+		fmt.Println(" -- Couldn't find a folder at " + path)
+		return
+	}
+	fmt.Println("Targeting: " + path)
+
 	fmt.Println("Reticulating splines")
 	fmt.Println("Downloading filter file")
 }
 
 func resolveenvironment() string {
 	return os.Getenv("UserProfile")
+}
+
+func direxists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	fmt.Println(err)
+	return false
 }
