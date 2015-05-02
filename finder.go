039f29d06aa49b5d34839eeaadc9b2a035d6920d
icbat <cobb.evan@gmail.com>
2015-05-01 21:37:08 -0500

    Laying out vague skeleton :dart:

---
+++ finder.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Checking environment")
+	fmt.Println("Verifying directories")
+	fmt.Println("Reticulating splines")
+	fmt.Println("Downloading filter file")
+}
