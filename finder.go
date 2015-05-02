3c87c677bbdfb1d8742596b3236b7b5e205274f9
icbat <cobb.evan@gmail.com>
2015-05-01 22:00:55 -0500

    Can get resolve a basic environment variable
    Who knows what versions of windows this works on? I sure don't...

--- finder.go
+++ finder.go
@@ -1,10 +1,22 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
 	fmt.Println("Checking environment")
+	basePath := resolveenvironment()
+	if basePath == "" {
+		fmt.Println(" -- Couldn't resolve environment variables!")
+		return
+	}
+	fmt.Println("Found base path of: " + basePath)
+
 	fmt.Println("Verifying directories")
 	fmt.Println("Reticulating splines")
 	fmt.Println("Downloading filter file")
+}
+
+func resolveenvironment() string {
+	return os.Getenv("UserProfile")
 }
