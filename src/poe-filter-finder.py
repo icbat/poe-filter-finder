abd087567aca949713e33bd78ff6a081d3b70c02
Evan Cobb <cobb.evan@gmail.com>
2016-01-24 17:56:35 -0600

    works to download just mine

--- src/poe-filter-finder.py
+++ src/poe-filter-finder.py
@@ -1,17 +1,33 @@
 import os
-
+import urllib.request
 
 def findPathInstallDirectory():
     userHome = os.path.expanduser("~")
-    installPath = userHome + "/Documents/My Games/Path of Exile"
+    installPath = userHome + "/Documents/My Games/Path of Exile/"
     print("Checking for Path at path " + installPath)
     if os.path.isdir(installPath):
         print("Found it at " + installPath)
+        return installPath
     else:
         raise FileNotFoundError("Couldn't find your Path of Exile installation. Checked at " + installPath)
 
+def findFilters():
+    filters = []
+    filters.append("https://gist.githubusercontent.com/icbat/a2376c23dbfca3f92bbb/raw/dbd42a0f15b66054dd94d9eff7a5aac05328bb16/icbat-loot.filter")
+    return filters
+
+def downloadAllFiltersTo(filters, installDirectory):
+    for url in filters:
+        urllib.request.urlretrieve(url, installDirectory + "test.filter")
+
 print("Finding your Path of Exile install")
 installDirectory = findPathInstallDirectory()
+
+print("Finding Filters")
+filters = findFilters()
+
 print("Downloading filter to there")
+downloadAllFiltersTo(filters, installDirectory)
+
 print("BOOM! Go get loot with prettier colors.")
 input("Press enter to exit")
