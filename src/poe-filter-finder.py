f0e729efe7cd64315b01847b154a089177d57c97
Evan Cobb <cobb.evan@gmail.com>
2016-01-24 17:41:18 -0600

    finds your directory in the happy path

--- src/poe-filter-finder.py
+++ src/poe-filter-finder.py
@@ -1,4 +1,17 @@
+import os
+
+
+def findPathInstallDirectory():
+    userHome = os.path.expanduser("~")
+    installPath = userHome + "/Documents/My Games/Path of Exile"
+    print("Checking for Path at path " + installPath)
+    if os.path.isdir(installPath):
+        print("Found it at " + installPath)
+    else:
+        raise FileNotFoundError("Couldn't find your Path of Exile installation. Checked at " + installPath)
+
 print("Finding your Path of Exile install")
+installDirectory = findPathInstallDirectory()
 print("Downloading filter to there")
 print("BOOM! Go get loot with prettier colors.")
 input("Press enter to exit")
