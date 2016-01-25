e5701eb85ae9d0a198a96f4d0889ff582962025a
Evan Cobb <cobb.evan@gmail.com>
2016-01-24 18:02:11 -0600

    using the filename from the url, adding messages around the actual filter grab

--- src/poe-filter-finder.py
+++ src/poe-filter-finder.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 
+
 def findPathInstallDirectory():
     userHome = os.path.expanduser("~")
     installPath = userHome + "/Documents/My Games/Path of Exile/"
@@ -11,14 +12,28 @@ def findPathInstallDirectory():
     else:
         raise FileNotFoundError("Couldn't find your Path of Exile installation. Checked at " + installPath)
 
+
 def findFilters():
     filters = []
-    filters.append("https://gist.githubusercontent.com/icbat/a2376c23dbfca3f92bbb/raw/dbd42a0f15b66054dd94d9eff7a5aac05328bb16/icbat-loot.filter")
+    filters.append(
+        "https://gist.githubusercontent.com/icbat/a2376c23dbfca3f92bbb/raw/dbd42a0f15b66054dd94d9eff7a5aac05328bb16/icbat-loot.filter")
     return filters
+
 
 def downloadAllFiltersTo(filters, installDirectory):
     for url in filters:
-        urllib.request.urlretrieve(url, installDirectory + "test.filter")
+        fileName = findFileName(url)
+        print("  Grabbing " + fileName + " from " + url)
+        urllib.request.urlretrieve(url, installDirectory + fileName)
+        print("  Successfully installed " + fileName)
+
+
+def findFileName(url):
+    split = str(url).split("/")
+    parts = len(split)
+
+    return split[parts - 1]
+
 
 print("Finding your Path of Exile install")
 installDirectory = findPathInstallDirectory()
